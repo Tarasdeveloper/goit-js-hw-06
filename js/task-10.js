710e59000e3383b160913f58a903d9cfc2a22c71
Tarasdeveloper <tomahavki1@gmail.com>
2023-04-18 20:53:48 +0300

    Updated 10

--- js/task-10.js
+++ js/task-10.js
@@ -7,10 +7,12 @@ function getRandomHexColor() {
 const controlsDiv = document.querySelector("#controls"); // Получение контейнера с кнопками
 const createButton = controlsDiv.querySelector("[data-create]"); // Получение кнопки "Create"
 const destroyButton = controlsDiv.querySelector("[data-destroy]"); // Получение кнопки "Destroy"
+const input = controlsDiv.querySelector("input"); // Получение input
 const boxesDiv = document.querySelector("#boxes"); // Получение контейнера для созданных элементов
 
 createButton.addEventListener("click", () => {
-  const amount = Number(controlsDiv.querySelector("input").value); // Получение количества элементов из input
+  const amount = Number(input.value); // Получение количества элементов из input
+  if (amount < 1 || amount > 100) return; // Проверка лимитов значения input
   createBoxes(amount); // Вызов функции создания элементов
 });
 
@@ -20,16 +22,15 @@ destroyButton.addEventListener("click", () => {
 
 function createBoxes(amount) {
   let size = 30; // Размер первого элемента
+  let boxes = ""; // Переменная для хранения созданных элементов в виде строки
   for (let i = 0; i < amount; i += 1) {
-    const box = document.createElement("div"); // Создание нового элемента div
-    box.style.width = `${size}px`; // Установка ширины элемента
-    box.style.height = `${size}px`; // Установка высоты элемента
-    box.style.backgroundColor = getRandomHexColor(); // Установка случайного цвета фона элемента
-    boxesDiv.appendChild(box); // Добавление элемента в контейнер для созданных элементов
+    boxes += `<div style="width:${size}px; height:${size}px; background-color:${getRandomHexColor()}"></div>`; // Использование шаблонной строки для создания элементов
     size += 10; // Увеличение размера для следующего элемента
   }
+  boxesDiv.innerHTML = boxes; // Добавление созданных элементов в контейнер для созданных элементов
 }
 
 function destroyBoxes() {
   boxesDiv.innerHTML = ""; // Очистка контейнера для созданных элементов
+  input.value = ""; // Очистка значения input
 }
